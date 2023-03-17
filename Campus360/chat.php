ea6fd37284335f9434f16d230b31e27f665c3900
ANDY\andre <andres.sanchez3b@gmail.com>
2023-03-17 11:42:07 +0100

    plantilla del chat

---
+++ Campus360/chat.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__.'/includes/config.php';
+
+$tituloPagina = 'Chat';
+$contenidoPrincipal=<<<EOS
+  <h1>Chats</h1>
+  <div class="vistaChats">
+  <div class="listaNombres">
+    <ul>
+      <li>Chat 1</li>
+      <li>Chat 2</li>
+      <li>Chat 3</li>
+      <li>Chat 4</li>
+    </ul>
+  </div>
+  <div class="chat">
+  <form action="enviar-correo.php" method="post">
+
+  <label for="asunto">Asunto:</label>
+  <input type="text" id="asunto" name="asunto">
+
+  <label for="mensaje">Mensaje:</label>
+  <textarea id="mensaje" name="mensaje"></textarea>
+
+  <button type="submit">Enviar mensaje</button>
+</form>
+
+  </div>
+</div>
+EOS;
+
+$params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal];
+$app->generaVista('/plantillas/plantillaChat.php', $params);
